91c5f60b2db961ccc750e61f47f2ffec8133194d
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2020-11-11 21:50:12 -0300

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,11 @@ menuCheck.addEventListener('click', () => {
   if (menuCheck.checked) {
     menuCont.classList.add('header-menu')
     menuBg.classList.add('body_open_menu')
+    document.body.classList.add('hidden')
   } else {
     menuCont.classList.remove('header-menu')
     menuBg.classList.remove('body_open_menu')
+    document.body.classList.remove('hidden')
   }
 })
 
@@ -17,4 +19,16 @@ menuBg.addEventListener('click', () => {
     menuBg.classList.remove('body_open_menu')
 
     menuCheck.checked = false
+})
+
+// scrool to top
+let scroolToTopBtn = document.querySelector('#btntop')
+let rootElement = document.documentElement
+
+scroolToTopBtn.addEventListener('click', () => {
+  // scroolo to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
 })
